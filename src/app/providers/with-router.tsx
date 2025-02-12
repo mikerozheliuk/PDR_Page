62792f7f592df85f91ebb062d6024092c2a253d9
mike <rozheluikmisha@gmail.com>
2025-02-12 20:44:32 +0200

    fix(components): fix style and add json files

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -9,10 +9,9 @@ import Tariffs from 'pages/tariffs';
 import Instructions from 'pages/instructions';
 import PrivateRoute from 'shared/ui/PrivateRoute';
 import { AuthProvider } from 'pages/auth/AuthContext';
+import Themes from 'pages/themes';
 
-type WithRouterProps = {
-  children?: React.ReactNode;
-};
+type WithRouterProps = { children?: React.ReactNode };
 
 export const withRouter =
   <T extends WithRouterProps>(Cmp: React.ComponentType<T>) =>
@@ -34,6 +33,7 @@ export const withRouter =
                   <Route path="/tariffs" element={<Tariffs />} />
                   <Route path="/instructions" element={<Instructions />} />
                   <Route path="/support" element={<Support />} />
+                  <Route path="/main/themes" element={<Themes />} />
                 </Route>
               </Route>
 
