b0c61fe9dc2848be75d11595b0e3f5c5322cbb38
Mykhailo <98221429+mikerozheliuk@users.noreply.github.com>
2025-02-16 19:20:43 +0200

    feat: create tests page and add  functional (#12)

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Auth, Home, Main, PageNotFound, Support, Theory } from 'pages';
+
 import { Layout } from 'shared/ui';
 import { PageLayout } from 'shared/ui/Layout';
+
+import Tests from 'pages/tests';
+import Themes from 'pages/themes';
 import Profile from 'pages/profile';
 import Tariffs from 'pages/tariffs';
 import Instructions from 'pages/instructions';
 import PrivateRoute from 'shared/ui/PrivateRoute';
 import { AuthProvider } from 'pages/auth/AuthContext';
-import Themes from 'pages/themes';
 
 type WithRouterProps = { children?: React.ReactNode };
 
@@ -31,9 +34,10 @@ export const withRouter =
                   <Route path="/theory" element={<Theory />} />
                   <Route path="/profile" element={<Profile />} />
                   <Route path="/tariffs" element={<Tariffs />} />
-                  <Route path="/instructions" element={<Instructions />} />
                   <Route path="/support" element={<Support />} />
+                  <Route path="/main/tests" element={<Tests />} />
                   <Route path="/main/themes" element={<Themes />} />
+                  <Route path="/instructions" element={<Instructions />} />
                 </Route>
               </Route>
 
